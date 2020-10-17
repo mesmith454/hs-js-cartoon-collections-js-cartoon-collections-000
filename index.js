f72cc70c6941d127a50a8cf5315d9e05b9921ec8
vivacious-revert-7013 <mesmith454@gmail.com>
2020-10-17 21:49:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//define function that takes array dwarves as argument, set empty array, instruct function to move incrementally thru array and push resulting message to empty array. should return dwarf index number plus one and the appropriate dwarf name, .join returns statement as a string
+
 var dwarves = ["Dopey", "Grumpy", "Bashful"];
 function dwarfRollCall(dwarves) {
   var array = [];
@@ -6,10 +8,14 @@ function dwarfRollCall(dwarves) {
   return array.join("");
 }
 
+//define a function to convert planeteerCalls to uppercase and add an exclamation point. .map creates a new array with results of calling function, .toUpperCase to convert character case
+
 var planeteerCalls = ["earth", "wind", "fire", "water", "heart"];
 function summonCaptainPlanet(planeteerCalls){
   return planeteerCalls.map(a=>a.toUpperCase() +"!");
 }
+
+//define a function to check length of words variables, if longer than four characters return true otherwise false. 3rd line of code tells function how to move thru array starting with index 0 incrementally, 4th instructs function to check character length
 
 var words = ["earth", "wind", "fire", "water", "air"];
 function longPlaneteerCalls(words) {
@@ -18,15 +24,24 @@ function longPlaneteerCalls(words) {
       return true;
     }
   }
-  return false
+  return false;
 }
 
-function findTheCheese (foods) {
+//define a function to search array foods for the following cheeses, if cheese is located return cheese located, other wise return no cheese. instruct function to move incrementally thru array, double pipes act as or operater
+
+var foods = ["cheddar", "gouda", "camembert"];
+function findTheCheese(foods) {
   for(var i = 0; i<foods.length; i++){
     if (foods[i]=="cheddar"
     ||foods[i]=="gouda"
-    ||foods[i]=="camembert")
+    ||foods[i]=="camembert"
+    ||foods[i]=="swiss")
     return foods[i];
   }
   return "no cheese!";
 }
+
+
+function wordsWithB(words) {
+  for(var i = 0; i<words; i++)
+  
